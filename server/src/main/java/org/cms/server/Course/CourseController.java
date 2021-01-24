89ba878e9632804c39e86a41ffe4a11454815270
Archit Singh <architsingh456@gmail.com>
2021-01-24 07:47:09 +0530

    UPD : added dummy rest controller

--- server/src/main/java/org/cms/server/Course/CourseController.java
+++ server/src/main/java/org/cms/server/Course/CourseController.java
@@ -1,2 +1,21 @@
-package org.cms.server.Course;public class CourseController {
+package org.cms.server.Course;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping("/courses")
+    public List<Course> getAllCourses(){
+        return Arrays.asList(
+                new Course(1,"Spring Framework","Spring Framework Description"),
+                new Course(2,"Maven ","Maven is a build tool"),
+                new Course(3,"CICD  ","Jenkins is a CICD pipelining tool")
+
+        );
+    }
 }
