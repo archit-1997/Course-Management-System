1555bdc3f3c25ea76fde07048790054b50487c0b
Archit Singh <architsingh456@gmail.com>
2021-01-25 23:05:29 +0530

    UPD : added dummy rest api's

--- server/src/main/java/org/cms/server/Course/CourseController.java
+++ server/src/main/java/org/cms/server/Course/CourseController.java
@@ -1,21 +1,25 @@
 package org.cms.server.Course;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class CourseController {
 
+    @Autowired
+    private CourseService courseService;
+
     @RequestMapping("/courses")
     public List<Course> getAllCourses(){
-        return Arrays.asList(
-                new Course(1,"Spring Framework","Spring Framework Description"),
-                new Course(2,"Maven ","Maven is a build tool"),
-                new Course(3,"CICD  ","Jenkins is a CICD pipelining tool")
+        return courseService.getAllCourses();
+    }
 
-        );
+    @RequestMapping("/course/{id}")
+    public Course getCourse(@PathVariable int id){
+        return courseService.getCourse(id);
     }
 }
